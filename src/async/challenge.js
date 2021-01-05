b8b9ea43d5be5126dcdfae2faea06aced9273093
Eliud <juarez_s3@hotmail.com>
2021-01-04 23:48:03 -0600

    Uso de async/await y fetch

---
+++ src/async/challenge.js
@@ -0,0 +1,32 @@
+const fetchData = require('../utils/fetchData');
+const API = 'https://rickandmortyapi.com/api/character/';
+
+const anotherFunction = async (url_api) => {
+    try {
+        const data = await fetchData(url_api);  
+        const character = await fetchData(`${API}${data.results[0].id}`);
+        const origin = await fetchData(character.origin.url);
+        console.log(data.info.count);
+        console.log(character.name);
+        console.log(origin.dimension);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+console.log('Before');
+anotherFunction(API);
+console.log('After');
+
+//Esta es una forma de hacer que async/await espere a varios llamados al vez:
+
+// async function foo() {
+// 	const [characters , episodes, locations] = await Promise.all([
+// 		getCharacters(),
+// 		getEpisodes(),
+// 		getLocations(),
+// 	]);
+// 	console.log(characters);
+// 	console.log(episodes);
+// 	console.log(locations);
+// }
